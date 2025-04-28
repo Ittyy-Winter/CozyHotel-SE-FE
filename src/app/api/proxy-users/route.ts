d2e0ffe6185b9a024422603661bc11b7ad0a6436
SinnyX <47661181+SinnyX@users.noreply.github.com>
2025-04-28 17:25:45 +0700

    fix bug pagination

--- src/app/api/proxy-users/route.ts
+++ src/app/api/proxy-users/route.ts
@@ -1,14 +1,31 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const page = searchParams.get('page') || '1';
-  const limit = searchParams.get('limit') || '6';
+  const token = req.headers.get("authorization");
+  const formattedToken = token?.startsWith("Bearer ")
+    ? token
+    : `Bearer ${token}`;
 
-  const targetUrl = `https://cozy-hotel-se-be.vercel.app/api/v1/hotels?page=${page}&limit=${limit}`;
+  const searchParams = req.nextUrl.searchParams.toString();
 
-  const response = await fetch(targetUrl);
-  const data = await response.json();
+  const url = `https://cozy-hotel-se-be.vercel.app/api/v1/accounts${
+    searchParams ? `?${searchParams}` : ""
+  }`;
 
-  return NextResponse.json(data);
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: formattedToken || "",
+      },
+    });
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (error) {
+    console.error("Proxy error:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
